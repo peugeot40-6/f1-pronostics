5b672eb68e3109abb2b25160037df8e51bd72dbd
peugeot40-6 <ccocquyt80@gmail.com>
2025-03-21 13:27:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import os
 from flask import Flask, render_template, request
+import os
+template_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+app = Flask(__name__, template_folder=template_dir)
 
 import pandas as pd
 
